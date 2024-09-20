e051028d11a8dade6df82ed3739c06677409b8a5
Rutug25 <gawad.r@northeastern.edu>
2024-09-19 23:43:53 -0400

    Add files via upload

---
+++ PersonsProfile/src/model/Person.java
@@ -0,0 +1,231 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rutugawad
+ */
+
+    public class Person {
+    // Attributes
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String driversLicenseNumber;
+    private String driversLicenseState;
+    private String ssn;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zip;
+    private String telephoneNumber;
+    private String faxNumber;
+    
+    // Additional attributes
+    private String dateOfBirth;
+    private String gender;
+    private String maritalStatus;
+    private String employmentStatus;
+    private String nationality;
+    private String preferredContactMethod;
+
+    // Constructor
+    public Person(String firstName, String lastName, String email, String phone, String driversLicenseNumber, String driversLicenseState, String ssn, String addressLine1, String addressLine2, String city, String state, String zip, String telephoneNumber, String faxNumber, String dateOfBirth, String gender, String maritalStatus, String employmentStatus, String nationality, String preferredContactMethod) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.driversLicenseNumber = driversLicenseNumber;
+        this.driversLicenseState = driversLicenseState;
+        this.ssn = ssn;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.telephoneNumber = telephoneNumber;
+        this.faxNumber = faxNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.maritalStatus = maritalStatus;
+        this.employmentStatus = employmentStatus;
+        this.nationality = nationality;
+        this.preferredContactMethod = preferredContactMethod;
+    }
+
+    public Person() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    // Getters and setters for all attributes
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriversLicenseNumber() {
+        return driversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String driversLicenseNumber) {
+        this.driversLicenseNumber = driversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return driversLicenseState;
+    }
+
+    public void setDriversLicenseState(String driversLicenseState) {
+        this.driversLicenseState = driversLicenseState;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    // Additional attributes' getters and setters
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getEmploymentStatus() {
+        return employmentStatus;
+    }
+
+    public void setEmploymentStatus(String employmentStatus) {
+        this.employmentStatus = employmentStatus;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPreferredContactMethod() {
+        return preferredContactMethod;
+    }
+
+    public void setPreferredContactMethod(String preferredContactMethod) {
+        this.preferredContactMethod = preferredContactMethod;
+    }
+}
+
+
+
+
+
